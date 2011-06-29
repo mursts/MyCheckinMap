8c90e5eb5cdb0b81f0cd63df0e931f5bb37f12e4
mursts <mursts@gmail.com>
2011-06-30 00:46:30 +0900

    プロジェクト作成

---
+++ src/com/mursts/android/mycheckinmap/MyCheckinMapActivity.java
@@ -0,0 +1,13 @@
+package com.mursts.android.mycheckinmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyCheckinMapActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
